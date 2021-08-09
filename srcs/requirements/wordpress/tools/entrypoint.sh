f6cb2d79f92b06c0a9b4c3605fb890e3b45ad530
BoyerDamien <dboyer@student.42.fr>
2021-08-09 10:59:13 +0200

    add env check && build image name

--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -1,4 +1,34 @@
 #! /bin/ash
+
+display_error(){
+    >&2 printf "\n${RED}Error: $1${NC}\n"
+}
+
+display_info(){
+    >&1 printf "\n${GREEN}$1${NC}\n"
+}
+
+check_null(){
+    if [[ -z $1 ]]
+    then
+        display_error "$2"
+        exit 1
+    fi
+}
+
+check_env(){
+    display_info "Checking env variables ..."
+    check_null "$DB_USER" "DB_USER env is not set"
+    check_null "$URL" "ADMIN_MAIL env is not set"
+    check_null "$DB_NAME" "DB_NAME env is not set"
+    check_null "$DB_HOST" "DB_HOST env is not set"
+    check_null "$DB_PASSWORD" "DB_PASSWORD env is not set"
+    check_null "$ADMIN_USER" "ADMIN_USER env is not set"
+    check_null "$ADMIN_PASSWORD" "ADMIN_PASSWORD env is not set"
+    check_null "$ADMIN_MAIL" "ADMIN_MAIL env is not set"
+}
+
+check_env
 if [ ! -f /var/www/wordpress/wp-config.php ];
 then
 	wp core download    --path=/var/www/wordpress
